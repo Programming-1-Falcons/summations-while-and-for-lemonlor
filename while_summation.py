235faf05e13f128e2d0d3bc7a47a1fe40e879070
lemonlor <lemonlor001@alpinesd.org>
2024-10-22 11:32:18 -0600

    while_summation.py
    hehehehha

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,9 @@
 #while summation code following directions from Readme
+number = int(input("what number do you want things to happen to?:"))
+
+total = 0
+x = 0 
+while x <= (number):
+    total = total + x
+    x = x + 1
+print (total)
